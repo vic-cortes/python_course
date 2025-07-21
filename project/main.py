87efead905957ede676c57da9526c90a49f2b9ef
Victor M. Cortes Figueroa <vmcortesf@gmail.com>
2025-07-20 23:53:28 -0500

    Remove Python version file and update dependencies in project files
    - Deleted `.python-version` file from `uv_example` directory.
    - Updated `pyproject.toml` to include a new development dependency group with `ipython`.
    - Modified `uv.lock` to reflect new package resolutions and added several new packages.

---
+++ project/main.py
@@ -0,0 +1,4 @@
+import requests
+
+HOME_DEPOT_URL = "https://www.homedepot.com.mx/b/linea-blanca-y-cocinas/lavadoras-y-secadoras/lavadoras"
+# https://www.homedepot.com.mx/p/maytag-lavadora-maytag-22-kg-load-go-he-7mmhw6621hw-151135
