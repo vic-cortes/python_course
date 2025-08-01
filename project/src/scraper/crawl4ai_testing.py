58b41393db1441a1b4920673361d34c32c96e383
Victor M. Cortes Figueroa <vmcortesf@gmail.com>
2025-07-31 21:26:58 -0500

    Add Liverpool product scraping functionality with JSON output
    Updated `crawl4ai_testing.py` to implement a product scraper for Liverpool's website. Added a JSON extraction strategy and configured the crawler to save results to a JSON file. This enhances data collection capabilities for product information.

--- project/src/scraper/crawl4ai_testing.py
+++ project/src/scraper/crawl4ai_testing.py
@@ -1,19 +1,55 @@
 import asyncio
+import json
 
-from crawl4ai import AsyncWebCrawler, BrowserConfig
+from crawl4ai import (
+    AsyncWebCrawler,
+    BrowserConfig,
+    CrawlerRunConfig,
+    JsonCssExtractionStrategy,
+)
+from crawl4ai.models import CrawlResultContainer
 
 browser_config = BrowserConfig(browser_type="firefox", headless=False)
+BASE_URL = "https://www.liverpool.com.mx"
+PRODUCT_URL = f"{BASE_URL}/tienda?s=lavadoras"
 
 
 async def main():
+    schema = {
+        "name": "Liverpool Product Scraper",
+        "baseSelector": "li.m-product__card",
+        "fields": [
+            {"name": "title", "selector": "h3.card-title", "type": "text"},
+            {"name": "brand", "selector": "h3.a-card-brand", "type": "text"},
+            {"name": "price", "selector": "p.a-card-price", "type": "text"},
+            {
+                "name": "details_url",
+                "selector": "a",
+                "type": "attribute",
+                "attribute": "href",
+            },
+        ],
+    }
+
+    strategy = JsonCssExtractionStrategy(schema)
 
     # Create an instance of AsyncWebCrawler
     async with AsyncWebCrawler(config=browser_config) as crawler:
         # Run the crawler on a URL
-        result = await crawler.arun(url="https://crawl4ai.com")
+        result: CrawlResultContainer = await crawler.arun(
+            url=PRODUCT_URL,
+            config=CrawlerRunConfig(extraction_strategy=strategy),
+        )
 
         # Print the extracted content
         print(result.markdown)
+
+        if result.success:
+            # Save the result to a JSON file
+            with open("liverpool_products.json", "w") as file:
+                dict_data = json.loads(result.extracted_content)
+                json.dump(dict_data, file, indent=4)
+            print("Data saved to liverpool_products.json")
 
 
 if __name__ == "__main__":
