78e57af53b16846d9d478576cb13e793580b4381
Victor M. Cortes Figueroa <vmcortesf@gmail.com>
2025-08-13 09:47:43 -0500

    Add product table and ETL schema to support new data structure
    - Updated Makefile to include migration command.
    - Introduced `EtlSchema` dataclass in `main.py` for ETL processing.
    - Added `Product` model in `clients.py` to define product attributes.

--- project/src/etl/main.py
+++ project/src/etl/main.py
@@ -1,3 +1,19 @@
+from dataclasses import dataclass
+
 from src.scraper import SUPPORTED_SCRAPERS
 
 print("Supported scrapers:", SUPPORTED_SCRAPERS)
+
+
+@dataclass
+class EtlSchema:
+    brand: str
+    color: str
+    price: float
+    url: str
+    rpm: str
+    product_type: str
+    model: str
+    ancho: str
+    alto: str
+    capacidad: str
--- project/src/models/clients.py
+++ project/src/models/clients.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Float, Integer, String
 
 from ..db.base_class import Base
 
@@ -12,3 +12,22 @@ class Client(Base):
 
     def __repr__(self):
         return f"<Client(id={self.id}, name={self.name})>"
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    brand = Column(String(255))
+    color = Column(String(255))
+    price = Column(Float)
+    url = Column(String(255))
+    rpm = Column(String(255))
+    product_type = Column(String(255))
+    model = Column(String(255))
+    ancho = Column(String(255))
+    alto = Column(String(255))
+    capacidad = Column(String(255))
+
+    def __repr__(self):
+        return f"<Product(id={self.id}, brand={self.brand}, price={self.price})>"
